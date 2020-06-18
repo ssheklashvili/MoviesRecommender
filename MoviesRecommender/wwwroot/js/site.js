02e1e1d241b40a2131c55b507263c9875457172e
sandro.sheklashvili <sandro.sheklashvili95@gmail.com>
2020-06-18 17:34:47 +0400

    Add Loading

--- MoviesRecommender/wwwroot/js/site.js
+++ MoviesRecommender/wwwroot/js/site.js
@@ -1,7 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ajaxStart(function () {
+    $("#loading").show();
+    $("#pageWrapper").hide();
+});
 
-// Write your JavaScript code.
+$(document).ajaxStop(function () {
+    $("#loading").hide();
+    $("#pageWrapper").show();
+});
 
 function searchMovie() {
     var searchValue = $("#search").val();
@@ -10,11 +15,10 @@ function searchMovie() {
     $.ajax({
         url: "Home/SearchMovie",
         method: "GET",
-        async: false,
         contentType: "application/json",
         data: { name: searchValue },
         success: function (response) {
-            $("#pageWrapper").html(response); 
+            $("#pageWrapper").html(response);
         }
     });
-}
+};
